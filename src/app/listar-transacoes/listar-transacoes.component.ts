ae26cce9106e0d72ada616fb23977b9ed0f90d7a
Roosevelt Franklin <roosevelt_franklin@hotmail.com>
2024-04-10 17:04:37 -0300

    Interpolação usando função

--- src/app/listar-transacoes/listar-transacoes.component.ts
+++ src/app/listar-transacoes/listar-transacoes.component.ts
@@ -8,7 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './listar-transacoes.component.scss'
 })
 export class ListarTransacoesComponent {
+  // INTERPOLAÇÃO DE VARIAVEIS
   public nome = 'Roosevelt'; 
   public titulo = 'Controle Financeiro';
   public boasVindas = 'Olá ' + this.nome + ', Bem vindo(a) ao nosso sistema!';
+
+  // INTERPOLAÇÃO USANDO RETORNO DE UMA FUNÇÃO
+  public receitas = 2000;
+  public despesas = 500;
+
+  public saldo() {
+    return this.receitas - this.despesas
+  }
+
 }
